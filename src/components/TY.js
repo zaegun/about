d93ebbe5ef8fd762fcb4e5730b1803a02ee362e7
sozae32 <admin@studioosmunda.com>
2023-07-12 15:33:47 -0700

    added redirect message

--- src/components/TY.js
+++ src/components/TY.js
@@ -18,6 +18,7 @@ function TY() {
         <section className="node min-h-screen w-3/4 mx-auto mb-10 sm:w-1/2 timer-class">
             <h1 className='head-fade text-center opacity-0'>Thank You!</h1>
             <h2 className='sub-head-fade text-center opacity-0'>I will get back to you as soon as I can!</h2>
+            <p className='sub-head-fade text-center opacity-0'>Redirecting to Main Page...</p>
         </section>
 
     );
