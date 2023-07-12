327848026302e4e6e27dc94aaf3750b3cdcd5e29
sozae32 <admin@studioosmunda.com>
2023-07-12 15:28:14 -0700

    Real fix for fade

--- src/components/TY.js
+++ src/components/TY.js
@@ -16,8 +16,8 @@ function TY() {
     })
     return(
         <section className="node min-h-screen w-3/4 mx-auto mb-10 sm:w-1/2 timer-class">
-            <h1 className='head-fade text-center'>Thank You!</h1>
-            <h2 className='sub-head-fade text-center'>I will get back to you as soon as I can!</h2>
+            <h1 className='head-fade text-center opacity-0'>Thank You!</h1>
+            <h2 className='sub-head-fade text-center opacity-0'>I will get back to you as soon as I can!</h2>
         </section>
 
     );
