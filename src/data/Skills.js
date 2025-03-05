19f8b8e28691eb7baa152e328f52e19bbcca8283
Glenn Barker <gdbarker@Glenns-Mac-mini.local>
2025-03-04 18:52:40 -0800

    Test Modification

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -1,388 +1,390 @@
 export default [
-    {
-        id: 1,
-        category: "IT",
-        title: "Windows Server (including Core)",
-        description: "",
-        }, 
-        
-        {
-        id: 2,
-        category: "IT",
-        title: "Windows Client  ",
-        description: "",
-        },
-        
-        {
-        id: 3,
-        category: "IT",
-        title: "Red Hat Linux ",
-        description: "",
-        },
-        
-        {
-        id: 4,
-        category: "IT",
-        title: "Debian Linux ",
-        description: "",
-        },
-        
-        {
-        id: 5,
-        category: "IT",
-        title: "Azure AD ",
-        description: "",
-        },
-        
-        {
-        id: 6,
-        category: "IT",
-        title: "System Administration ",
-        description: "",
-        },
-        
-        {
-        id: 7,
-        category: "IT",
-        title: "Group Policy ",
-        description: "",
-        },
-        
-        {
-        id: 8,
-        category: "IT",
-        title: "DNS Server ",
-        description: "",
-        },
-        
-        {
-        id: 9,
-        category: "IT",
-        title: "Bash ",
-        description: "",
-        },
-        
-        {
-        id: 10,
-        category: "IT",
-        title: "PowerShell ",
-        description: "",
-        },
-        
-        {
-        id: 11,
-        category: "IT",
-        title: "Cisco IOS CLI ",
-        description: "",
-        },
-        
-        {
-        id: 12,
-        category: "IT",
-        title: "Excel with Macros",
-        description: "",
-        },
-        
-        {
-        id: 13,
-        category: "IT",
-        title: "Proxmox ",
-        description: "",
-        },
-        
-        {
-        id: 14,
-        category: "IT",
-        title: "Hyper-V ",
-        description: "",
-        },
-        
-        {
-        id: 15,
-        category: "IT",
-        title: "VMWare ",
-        description: "",
-        },
-        
-        {
-        id: 16,
-        category: "IT",
-        title: "Intune ",
-        description: "",
-        },
-        
-        {
-        id: 17,
-        category: "IT",
-        title: "Network Administration ",
-        description: "",
-        },
-        
-        {
-        id: 18,
-        category: "IT",
-        title: "Docker ",
-        description: "",
-        },
-        
-        {
-        id: 19,
-        category: "IT",
-        title: "Wireshark ",
-        description: "",
-        },
-        
-        {
-        id: 20,
-        category: "IT",
-        title: "Apache  ",
-        description: "",
-        },
-        
-        {
-        id: 21,
-        category: "IT",
-        title: "Microsoft 365 ",
-        description: "",
-        },
-        
-        {
-        id: 22,
-        category: "IT",
-        title: "SharePoint ",
-        description: "",
-        },
-        
-        {
-        id: 23,
-        category: "IT",
-        title: "Microsoft Teams ",
-        description: "",
-        },
-        {
-            id: 24,
-            category: "PRGM",
-            title: "Python",
-            description: "",
-            }, 
-            {
-            id: 25,
-            category: "PRGM",
-            title: "JavaScript",
-            description: "",
-            },
-            {
-            id: 26,
-            category: "PRGM",
-            title: "C#",
-            description: "",
-            },
-            {
-            id: 27,
-            category: "PRGM",
-            title: "Django ",
-            description: "",
-            },
-            {
-            id: 28,
-            category: "PRGM",
-            title: "React",
-            description: "",
-            },
-            {
-            id: 29,
-            category: "PRGM",
-            title: "TypeScript ",
-            description: "",
-            },
-            {
-            id: 30,
-            category: "PRGM",
-            title: "GDScript",
-            description: "",
-            },
-            {
-            id: 31,
-            category: "PRGM",
-            title: "Godot",
-            description: "",
-            },
-            {
-            id: 32,
-            category: "PRGM",
-            title: "Unity",
-            description: "",
-            },
-            {
-            id: 33,
-            category: "PRGM",
-            title: "HTML",
-            description: "",
-            },
-            {
-            id: 34,
-            category: "PRGM",
-            title: "CSS ",
-            description: "",
-            },
-            {
-            id: 35,
-            category: "PRGM",
-            title: "SQL",
-            description: "",
-            },
-            {
-            id: 36,
-            category: "PRGM",
-            title: "Kotlin",
-            description: "",
-            },
-            {
-            id: 37,
-            category: "PRGM",
-            title: "Visual Basic",
-            description: "",
-            },
-            {
-            id: 38,
-            category: "PRGM",
-            title: "PHP",
-            description: "",
-            },
-            {
-            id: 39,
-            category: "PRGM",
-            title: "Full Stack Web Development",
-            description: "",
-            },
-            {
-            id: 40,
-            category: "PRGM",
-            title: "iOS App Development ",
-            description: "",
-            },
-            {
-            id: 41,
-            category: "PRGM",
-            title: "Android App Development",
-            description: "",
-            },
-            {
-            id: 42,
-            category: "PRGM",
-            title: "Pascal",
-            description: "",
-            },
-            {
-            id: 43,
-            category: "PRGM",
-            title: "Bash Scripting",
-            description: "",
-            },
-            {
-            id: 44,
-            category: "PRGM",
-            title: "Git",
-            description: "",
-            },
-            {
-            id: 45,
-            category: "DSGN",
-            title: "Adobe Photoshop ",
-            description: "",
-            },
-            {
-            id: 46,
-            category: "DSGN",
-            title: "Adobe Illustrator ",
-            description: "",
-            },
-            {
-            id: 47,
-            category: "DSGN",
-            title: "Adobe InDesign",
-            description: "",
-            },
-            {
-            id: 48,
-            category: "DSGN",
-            title: "Affinity Photo",
-            description: "",
-            },            {
-            id: 49,
-            category: "DSGN",
-            title: "Affinity Designer",
-            description: "",
-            },            {
-            id: 50,
-            category: "DSGN",
-            title: "Affinity Publisher",
-            description: "",
-            },            {
-            id: 51,
-            category: "DSGN",
-            title: "Microsoft Publisher",
-            description: "",
-            },
-            {
-            id: 52,
-            category: "DSGN",
-            title: "Gimp",
-            description: "",
-            },
-            {
-            id: 53,
-            category: "DSGN",
-            title: "Blender",
-            description: "",
-            },
-            {
-            id: 54,
-            category: "DSGN",
-            title: "Figma",
-            description: "",
-            }, 
-            {
-            id: 55,
-            category: "DSGN",
-            title: "Procreate",
-            description: "",
-            },
-            {
-            id: 56,
-            category: "BIZ",
-            title: "QuickBooks Online",
-            description: "",
-            },      
-            {
-            id: 57,
-            category: "BIZ",
-            title: "Small Business Accounting",
-            description: "",
-            },     
-            {
-            id: 58,
-            category: "BIZ",
-            title: "Employee Insurance Benefit Consulting",
-            description: "",
-            },     
-            {
-            id: 59,
-            category: "BIZ",
-            title: "Property and Casualty Licensed (WA)",
-            description: "",
-            },     
-            {
-            id: 60,
-            category: "BIZ",
-            title: "Business Management",
-            description: "",
-            },     
-            {
-            id: 61,
-            category: "BIZ",
-            title: "Digital and Print Marketing",
-            description: "",
-            },                                
-]
+  {
+    id: 1,
+    category: "IT",
+    title: "Windows Server (including Core)",
+    description: "",
+  },
 
+  {
+    id: 2,
+    category: "IT",
+    title: "Windows Client  ",
+    description: "",
+  },
+
+  {
+    id: 3,
+    category: "IT",
+    title: "Red Hat Linux ",
+    description: "",
+  },
+
+  {
+    id: 4,
+    category: "IT",
+    title: "Debian Linux ",
+    description: "",
+  },
+
+  {
+    id: 5,
+    category: "IT",
+    title: "Azure AD ",
+    description: "",
+  },
+
+  {
+    id: 6,
+    category: "IT",
+    title: "System Administration ",
+    description: "",
+  },
+
+  {
+    id: 7,
+    category: "IT",
+    title: "Group Policy ",
+    description: "",
+  },
+
+  {
+    id: 8,
+    category: "IT",
+    title: "DNS Server ",
+    description: "",
+  },
+
+  {
+    id: 9,
+    category: "IT",
+    title: "Bash ",
+    description: "",
+  },
+
+  {
+    id: 10,
+    category: "IT",
+    title: "PowerShell ",
+    description: "",
+  },
+
+  {
+    id: 11,
+    category: "IT",
+    title: "Cisco IOS CLI ",
+    description: "",
+  },
+
+  {
+    id: 12,
+    category: "IT",
+    title: "Excel with Macros",
+    description: "",
+  },
+
+  {
+    id: 13,
+    category: "IT",
+    title: "Proxmox ",
+    description: "",
+  },
+
+  {
+    id: 14,
+    category: "IT",
+    title: "Hyper-V ",
+    description: "",
+  },
+
+  {
+    id: 15,
+    category: "IT",
+    title: "VMWare ",
+    description: "",
+  },
+
+  {
+    id: 16,
+    category: "IT",
+    title: "Configuration Maanger (SCCM) ",
+    description: "",
+  },
+
+  {
+    id: 17,
+    category: "IT",
+    title: "Network Administration ",
+    description: "",
+  },
+
+  {
+    id: 18,
+    category: "IT",
+    title: "Docker ",
+    description: "",
+  },
+
+  {
+    id: 19,
+    category: "IT",
+    title: "Wireshark ",
+    description: "",
+  },
+
+  {
+    id: 20,
+    category: "IT",
+    title: "Apache  ",
+    description: "",
+  },
+
+  {
+    id: 21,
+    category: "IT",
+    title: "Microsoft 365 ",
+    description: "",
+  },
+
+  {
+    id: 22,
+    category: "IT",
+    title: "SharePoint ",
+    description: "",
+  },
+
+  {
+    id: 23,
+    category: "IT",
+    title: "Microsoft Teams ",
+    description: "",
+  },
+  {
+    id: 24,
+    category: "PRGM",
+    title: "Python",
+    description: "",
+  },
+  {
+    id: 25,
+    category: "PRGM",
+    title: "JavaScript",
+    description: "",
+  },
+  {
+    id: 26,
+    category: "PRGM",
+    title: "C#",
+    description: "",
+  },
+  {
+    id: 27,
+    category: "PRGM",
+    title: "Django ",
+    description: "",
+  },
+  {
+    id: 28,
+    category: "PRGM",
+    title: "React",
+    description: "",
+  },
+  {
+    id: 29,
+    category: "PRGM",
+    title: "TypeScript ",
+    description: "",
+  },
+  {
+    id: 30,
+    category: "PRGM",
+    title: "GDScript",
+    description: "",
+  },
+  {
+    id: 31,
+    category: "PRGM",
+    title: "Godot",
+    description: "",
+  },
+  {
+    id: 32,
+    category: "PRGM",
+    title: "Unity",
+    description: "",
+  },
+  {
+    id: 33,
+    category: "PRGM",
+    title: "HTML",
+    description: "",
+  },
+  {
+    id: 34,
+    category: "PRGM",
+    title: "CSS ",
+    description: "",
+  },
+  {
+    id: 35,
+    category: "PRGM",
+    title: "SQL",
+    description: "",
+  },
+  {
+    id: 36,
+    category: "PRGM",
+    title: "Kotlin",
+    description: "",
+  },
+  {
+    id: 37,
+    category: "PRGM",
+    title: "Visual Basic",
+    description: "",
+  },
+  {
+    id: 38,
+    category: "PRGM",
+    title: "PHP",
+    description: "",
+  },
+  {
+    id: 39,
+    category: "PRGM",
+    title: "Full Stack Web Development",
+    description: "",
+  },
+  {
+    id: 40,
+    category: "PRGM",
+    title: "iOS App Development ",
+    description: "",
+  },
+  {
+    id: 41,
+    category: "PRGM",
+    title: "Android App Development",
+    description: "",
+  },
+  {
+    id: 42,
+    category: "PRGM",
+    title: "Pascal",
+    description: "",
+  },
+  {
+    id: 43,
+    category: "PRGM",
+    title: "Bash Scripting",
+    description: "",
+  },
+  {
+    id: 44,
+    category: "PRGM",
+    title: "Git",
+    description: "",
+  },
+  {
+    id: 45,
+    category: "DSGN",
+    title: "Adobe Photoshop ",
+    description: "",
+  },
+  {
+    id: 46,
+    category: "DSGN",
+    title: "Adobe Illustrator ",
+    description: "",
+  },
+  {
+    id: 47,
+    category: "DSGN",
+    title: "Adobe InDesign",
+    description: "",
+  },
+  {
+    id: 48,
+    category: "DSGN",
+    title: "Affinity Photo",
+    description: "",
+  },
+  {
+    id: 49,
+    category: "DSGN",
+    title: "Affinity Designer",
+    description: "",
+  },
+  {
+    id: 50,
+    category: "DSGN",
+    title: "Affinity Publisher",
+    description: "",
+  },
+  {
+    id: 51,
+    category: "DSGN",
+    title: "Microsoft Publisher",
+    description: "",
+  },
+  {
+    id: 52,
+    category: "DSGN",
+    title: "Gimp",
+    description: "",
+  },
+  {
+    id: 53,
+    category: "DSGN",
+    title: "Blender",
+    description: "",
+  },
+  {
+    id: 54,
+    category: "DSGN",
+    title: "Figma",
+    description: "",
+  },
+  {
+    id: 55,
+    category: "DSGN",
+    title: "Procreate",
+    description: "",
+  },
+  {
+    id: 56,
+    category: "BIZ",
+    title: "QuickBooks Online",
+    description: "",
+  },
+  {
+    id: 57,
+    category: "BIZ",
+    title: "Small Business Accounting",
+    description: "",
+  },
+  {
+    id: 58,
+    category: "BIZ",
+    title: "Employee Insurance Benefit Consulting",
+    description: "",
+  },
+  {
+    id: 59,
+    category: "BIZ",
+    title: "Property and Casualty Licensed (WA)",
+    description: "",
+  },
+  {
+    id: 60,
+    category: "BIZ",
+    title: "Business Management",
+    description: "",
+  },
+  {
+    id: 61,
+    category: "BIZ",
+    title: "Digital and Print Marketing",
+    description: "",
+  },
+];
